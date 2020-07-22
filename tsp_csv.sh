bc303156b8e762ffbfb54991c1910768ae97e461
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2020-07-22 18:22:20 -0400

    Update script for new TSP website web request format.

--- tsp_csv.sh
+++ tsp_csv.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cd /data
-curl --data "whichButton=CSV" https://www.tsp.gov/InvestmentFunds/FundPerformance/index.html | sed '1d' > tspsharePriceHistory.csv
-
-cat tspsharePriceHistory.csv tspsharePriceHistoryMax.csv > tmp
-cat tmp | awk -F ',' 'BEGIN {ORS=""; maxF = 0; max[2]=0} NF {if (NF > maxF) {maxF = NF} for (i=1; i < NF; i++) { print $i "," ; if (i > 1 && $i+0>0 && $i > max[i]) {max[i] = $i} } ; print "\n" ;} END {print "Max,"; for (i=2;i<maxF;i++) { print max[i] ","} print "\n" }' | tail -n 1 > tspsharePriceHistoryMax.csv
+date=$(date "+%Y%m%d")
+daily=tspsharePriceHistory${date}.csv
+out=tspsharePriceHistory.csv
+max=tspsharePriceHistoryMax.csv
+curl "https://secure.tsp.gov/components/CORS/getSharePrices.html?InvFunds=1&format=CSV&download=1" | sed '1d' > $daily
+cp $daily $out
+cat $out $max > tmp
+cat tmp | awk -F ',' 'BEGIN {ORS=""; maxF = 0; max[2]=0} NF {if (NF > maxF) {maxF = NF} for (i=1; i < NF; i++) { print $i "," ; if (i > 1 && $i+0>0 && $i > max[i]) {max[i] = $i} } ; print "\n" ;} END {print "Max,"; for (i=2;i<maxF;i++) { print max[i] ","} print "\n" }' | tail -n 1 > $max
 rm tmp
 exit
